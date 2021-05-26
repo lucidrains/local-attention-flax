ceb8d214a38524774cf62d0aa5622147426b28bb
Phil Wang <lucidrains@gmail.com>
2021-05-25 23:10:46 -0700

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-## Local Attention - Jax (wip)
+## Local Attention - Flax
 
-Local Attention - Flax module in Jax
+Autoregressive Local Attention - Flax module for Jax
+
+## Install
+
+```bash
+$ pip install local-attention-flax
+```
+
+## Usage
+
+```python
+from jax import numpy as np, random
+from local_attention_flax import LocalAttention
+
+attn = LocalAttention(
+    dim = 256,
+    dim_head = 64,
+    heads = 8,
+    window_size = 128
+)
+
+key = random.PRNGKey(0)
+x = random.normal(key, (512, 256))
+
+params = attn.init(key, x)
+out = attn.apply(params, x)  # (512, 256)
+```
--- local_attention_flax/local_attention_flax.py
+++ local_attention_flax/local_attention_flax.py
@@ -2,6 +2,8 @@ import flax.linen as nn
 from jax import numpy as np
 from einops import rearrange
 
+ATTN_MASK_VALUE = -1e10
+
 class LocalAttention(nn.Module):
     dim: int
     window_size: int
@@ -10,4 +12,25 @@ class LocalAttention(nn.Module):
 
     @nn.compact
     def __call__(self, x):
-        return x
+        n, h, dim_head, wsz = x.shape[0], self.heads, self.dim_head, self.window_size
+        assert (n % wsz) == 0, 'sequence length must be divisible by the window size'
+        scale = dim_head ** -0.5
+        window = n // wsz
+
+        qkv = nn.Dense(features = 3 * h * dim_head)(x)
+        q, k, v = np.split(qkv, 3, axis = -1)
+        q, k, v = map(lambda t: rearrange(t, '(w n) (h d) -> h w n d', w = window, h = h), (q, k, v))
+
+        k, v = map(lambda t: np.pad(t, ((0, 0), (1, 0), (0, 0), (0, 0)), constant_values = 0.), (k ,v))
+        k, v = map(lambda t: np.concatenate((t[:, :-1], t[:, 1:]), axis = 2), (k, v))
+
+        sim = np.einsum('h w i d, h w j d -> h w i j', q, k) * scale
+
+        mask = np.tril(np.ones((wsz, wsz * 2)), wsz)
+        sim = np.where(mask, sim, ATTN_MASK_VALUE)
+
+        attn = nn.softmax(sim, axis = -1)
+        out = np.einsum('h w i j, h w j d -> h w i d', attn, v)
+        out = rearrange(out, 'h w n d -> (w n) (h d)')
+        out =  nn.Dense(features = self.dim)(out)
+        return out
